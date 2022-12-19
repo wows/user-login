0ef2d2a3dd396e1f84ec83eb57b9c6858243c2a6
wows <113129819+wows@users.noreply.github.com>
2022-12-19 10:37:11 -0700

    h

--- main.js
+++ main.js
@@ -2,16 +2,5 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  let username1 = document.getElementById("username").value.toLowerCase();
-  let password2 = document.getElementById("password").value.toLowerCase();
-  console.log(username1);
-  if (username1 === "Admin") {
-    if (password2 === "1234") {
-      alert("login successful");
-    } else {
-      alert("invalid password");
-    }
-  } else {
-    alert("invalid username");
-  }
+  console.log("hi");
 }
